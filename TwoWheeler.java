abbe43e3c7e2ad8fe471a06c7d6a9aac8bb0a5b1
manisankaren <115526014+manisankaren@users.noreply.github.com>
2022-10-11 11:33:01 +0530

    Add files via upload

---
+++ TwoWheeler.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class TwoWheeler {
+	public static void main(String[] args) {
+		int noofwheels=2;
+		System.out.println(noofwheels);
+		double TwoWheelerprice=20000;
+		System.out.println(TwoWheelerprice);
+		char firstLetter='T';
+		System.out.println(firstLetter);
+		float frequency=33.33893f;
+		System.out.println(frequency);
+		long registrationNumber=5655;
+		System.out.println(registrationNumber);
+	}
+
+}
